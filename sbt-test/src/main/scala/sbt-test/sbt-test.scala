7c0cddc70d729c62f14ee5e50cd28d1294be3af5
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-02-11 23:28:29 +0000

    sbt-test project with dependencies on standard libraries

---
+++ sbt-test/src/main/scala/sbt-test/sbt-test.scala
@@ -0,0 +1,25 @@
+/*
+sbt-test.scala
+
+
+*/
+
+object SbtTest {
+
+  def main(args: Array[String]): Unit = {
+    println("")
+    println("")
+    println("")
+    println("")
+    println("")
+    println("SBT IS INSTALLED AND WORKING")
+    println("")
+    println("")
+    println("")
+  }
+
+}
+
+/* eof */
+
+
