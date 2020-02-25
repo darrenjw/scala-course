85ba3c552a27eeafd123141b8c11d0fdca39630f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2020-02-25 15:08:47 +0000

    messing with tests

--- sbt-test/src/test/scala/sbt-test-scalacheck.scala
+++ sbt-test/src/test/scala/sbt-test-scalacheck.scala
@@ -1,27 +1,58 @@
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-import org.scalacheck.Prop.forAll
+import org.scalacheck._
+import org.scalacheck.Prop.{forAll,propBoolean}
 
-class StringSpecification extends AnyFlatSpec with Matchers {
+class SqrtSpecification extends Properties("Sqrt") with Matchers {
 
-  "Concatenated strings" should "startwith first string" in {
+  property("math.sqrt should square to give original") =
+    forAll { a: Double =>
+      (a >= 0.0) ==> {
+        val s = math.sqrt(a)
+        val tol = 1e-8 * a
+        s*s === a +- tol
+      }
+    }
+
+}
+
+class GammaSpec extends Properties("Gamma") with Matchers {
+
+  import breeze.stats.distributions.Gamma
+
+  val tol = 1e-8
+  val big = 1e100
+
+  property("mean") =
+    forAll { (a: Double, b: Double) =>
+      ((a > tol) && (a < big) && (b > tol) && (b < big)) ==> {
+        Gamma(a,b).mean === a*b +- tol
+      }
+    }
+
+}
+
+class StringSpecification extends Properties("String") with Matchers {
+
+  property("startwith first string") =
     forAll { (a: String, b: String) =>
       (a+b).startsWith(a)
     } 
-  }
 
-  it should "concatenate" in {
+  property("concatenate bound") =
     forAll { (a: String, b: String) =>
       (a+b).length >= a.length && (a+b).length >= b.length
     }
-  }
 
-  it should "substring" in {
+  property("concatenate length") =
+    forAll { (a: String, b: String) =>
+      (a+b).length == a.length + b.length
+    }
+
+  property("substring") =
     forAll { (a: String, b: String, c: String) =>
       (a+b+c).substring(a.length, a.length+b.length) == b
     }
-  }
 
 }
 
